9fa5746b55469033483812367618c4d4b0ada6ff
sekharp2323 <sekharchandrap23@gmail.com>
2023-04-15 19:59:57 +0530

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 101));
+  const [guess, setGuess] = useState('');
+  const [attempts, setAttempts] = useState(0);
+  const [message, setMessage] = useState('All the best! Start the game by clicking button');
+
+  const generate = () => {
+    const newrandomNumber = Math.floor(Math.random() * 101);
+    console.log(newrandomNumber);
+    setRandomNumber(newrandomNumber);
+  };
+  useEffect(()=>{
+    console.log(randomNumber);
+  },[randomNumber])
+
+  const check = () => {
+    const diff = Math.abs(randomNumber - guess);
+    setAttempts(prevAttempts => prevAttempts + 1); // use functional update
+    if (diff === 0) {
+      if (attempts < 5) {
+        setMessage(`Great! You guessed quite fast. You guessed it in ${attempts + 1} attempts.`); // update message to use updated attempts value
+        generate();
+        setAttempts(0);
+      } else if (attempts < 10) {
+        setMessage(`Good! You guessed not too fast. You guessed it in ${attempts + 1} attempts.`); // update message to use updated attempts value
+        generate();
+        setAttempts(0);
+      } else {
+        setMessage(`God! You guessed at last. You guessed it in ${attempts + 1} attempts.`); // update message to use updated attempts value
+        generate();
+        setAttempts(0);
+      }
+    } else if (diff <= 5) {
+      setMessage('You are around five.');
+    } else if (diff <= 15) {
+      setMessage('You are around fifteen.');
+    } else {
+      setMessage('You are quite away.');
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='head'>
+        <h1>Guess The Number!!!</h1>
+      </div>
+      <div className='body'>
+        <input
+          type='number'
+          placeholder='Enter your guess number'
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              check();
+            }
+          }}
+        />
+        <button onClick={generate}>Generate Random Number</button>
+        <h3>{message}</h3>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
